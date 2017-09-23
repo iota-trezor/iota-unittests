2ee81128c61e3a26e7f591c5c2cb557972592835
Bart Slinger <bartslinger@gmail.com>
2017-09-23 22:29:31 +0200

    trytes to trits conversion

--- tests/conversion_test.c
+++ tests/conversion_test.c
@@ -52,9 +52,64 @@ void test_MultipleTritsToTrytes(void)
     TEST_ASSERT_EQUAL(0, ret);
 }
 
+void test_Int32ToTrits(void)
+{
+    int32_t value = 10;
+    trit_t trits_out[10] = {0};
+
+    int ret;
+    ret = int32_to_trits(value, trits_out, 10);
+
+    TEST_ASSERT_EQUAL(1, trits_out[0]);
+    TEST_ASSERT_EQUAL(0, trits_out[1]);
+    TEST_ASSERT_EQUAL(1, trits_out[2]);
+    // No error
+    TEST_ASSERT_EQUAL(0, ret);
+
+    value = -17;
+    ret = int32_to_trits(value, trits_out, 10);
+
+    TEST_ASSERT_EQUAL(1, trits_out[0]);
+    TEST_ASSERT_EQUAL(0, trits_out[1]);
+    TEST_ASSERT_EQUAL(1, trits_out[2]);
+    TEST_ASSERT_EQUAL(-1, trits_out[3]);
+    // No error
+    TEST_ASSERT_EQUAL(0, ret);
+}
+
+void test_Int32ToTritsMaxLengthExceeded()
+{
+    int32_t value = -17;
+    trit_t trits_out[10] = {0};
+
+    int ret;
+    ret = int32_to_trits(value, trits_out, 3);
+
+    // Values are partly written
+    TEST_ASSERT_EQUAL(1, trits_out[0]);
+    TEST_ASSERT_EQUAL(0, trits_out[1]);
+    TEST_ASSERT_EQUAL(1, trits_out[2]);
+    TEST_ASSERT_EQUAL(0, trits_out[3]); // This one is not written
+
+    // Max length exceeded, give error
+    TEST_ASSERT_EQUAL(-1, ret);
+}
+
 void test_TrytesToTrits(void)
 {
-    TEST_ASSERT_TRUE(0);
+    tryte_t trytes_in[2] = {-5, 13}; // [1,1,-1] [1,1,1]
+    trit_t trits_out[6] = {0};
+
+    int ret;
+    ret = trytes_to_trits(trytes_in, trits_out, 2);
+
+    TEST_ASSERT_EQUAL(1, trits_out[0]);
+    TEST_ASSERT_EQUAL(1, trits_out[1]);
+    TEST_ASSERT_EQUAL(-1, trits_out[2]);
+    TEST_ASSERT_EQUAL(1, trits_out[3]);
+    TEST_ASSERT_EQUAL(1, trits_out[4]);
+    TEST_ASSERT_EQUAL(1, trits_out[5]);
+    TEST_ASSERT_EQUAL(0, ret);
 }
 
 int main(void)
@@ -64,6 +119,8 @@ int main(void)
     RUN_TEST(test_ThreeTritsToSingleTryte);
     RUN_TEST(test_InvalidNumberOfTrits);
     RUN_TEST(test_MultipleTritsToTrytes);
+    RUN_TEST(test_Int32ToTrits);
+    RUN_TEST(test_Int32ToTritsMaxLengthExceeded);
     RUN_TEST(test_TrytesToTrits);
 
     return UNITY_END();
